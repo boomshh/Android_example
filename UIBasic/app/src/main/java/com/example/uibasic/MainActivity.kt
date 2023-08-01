d259cc38a08d1cb28840f80e2dbc1dcefec46eb9
Ethan <fanfirst4@naver.com>
2023-08-01 16:48:32 +0900

    practice

---
+++ UIBasic/app/src/main/java/com/example/uibasic/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.uibasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.text_views)
+
+//        fun doAction(v:View) {
+//            Toast.makeText(getApplicationContext(), "Submitted Successfully",
+//                Toast.LENGTH_SHORT).show();
+//        }
+
+        val btn = findViewById<Button>(R.id.submit_button)
+
+        btn.setOnClickListener {
+            Toast.makeText(this, "Submited Successfully", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
