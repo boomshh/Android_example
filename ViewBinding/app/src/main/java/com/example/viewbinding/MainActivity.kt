81dabae59e215eaaf7ff8bd23ecda9445d441345
Ethan <fanfirst4@naver.com>
2023-08-22 14:55:14 +0900

    ViewBinding

---
+++ ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+
+        binding.btn.setOnClickListener {
+            binding.textView.text = "hi"
+        }
+    }
+}
