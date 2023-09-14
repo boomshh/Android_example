e7544952f82e4a5b63240aedbf021d7a63c0e439
Ethan <fanfirst4@naver.com>
2023-09-14 15:25:28 +0900

    setting xml

---
+++ save/settingsxml/src/main/java/com/example/settingsxml/BlankFragment.kt
@@ -0,0 +1,15 @@
+package com.example.settingsxml
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
+
+
+class BlankFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+    }
+}
